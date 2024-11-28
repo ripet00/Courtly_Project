fe2ff8fb45b98c1e8bdba859818d5719a2a8d21c
Resalfin <resalfin88@gmail.com>
2024-11-28 09:57:19 +0800

    Fix : Navigation

--- app/src/main/java/com/example/courtlyproject/view/MainActivity.kt
+++ app/src/main/java/com/example/courtlyproject/view/MainActivity.kt
@@ -11,12 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.courtlyproject.AuthViewModel
-import com.example.courtlyproject.MyAppNavigation
-import com.example.courtlyproject.SplashScreen
+import com.example.courtlyproject.controller.MyAppNavigation
 import com.example.courtlyproject.ui.theme.CourtlyProjectTheme
 
 class MainActivity : ComponentActivity() {
