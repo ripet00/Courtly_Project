151cbefa9825c48d67f9f248ba5f3940baa9a4ac
Rifat <rifat.khaidir@gmail.com>
2024-12-05 16:11:46 +0800

    feat: get lapanganId from myAppNavigation, fix function, & receive data from firestore

--- app/src/main/java/com/example/courtlyproject/Feature/detail/viewmodel/detaillapangan_vm.kt
+++ app/src/main/java/com/example/courtlyproject/Feature/detail/viewmodel/detaillapangan_vm.kt
@@ -10,32 +10,41 @@ import com.google.firebase.firestore.FirebaseFirestoreException
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
-class detaillapangan_vm: ViewModel() {
+class detaillapangan_vm(): ViewModel() {
     val state = mutableStateOf(detaillapangan())
 
-    init {
-        getDetail()
-    }
 
-    private fun getDetail() {
+    fun getDetail(lapanganId: String) {
         viewModelScope.launch {
-            state.value = getDetailFromFireStore()
+            state.value = getDetailFromFireStore(lapanganId = lapanganId)
         }
+    }
+
+    suspend fun getDetailFromFireStore(lapanganId: String):detaillapangan {
+
+        val db = FirebaseFirestore.getInstance()
+        var detail = detaillapangan()
+
+        try {
+            val result = db.collection("detail")
+                .document(lapanganId)
+                .get()
+                .await()
+
+            if (result.exists()) {
+                // jika dokumen ditemukan, konversikan menjadi objek detail lapangan
+                detail = result.toObject(detaillapangan::class.java) ?: detaillapangan()
+            } else {
+                // jika dokumen tidak ditemukan
+                Log.d("detaillapangan", "lapangan dengan ID : $lapanganId  tidak ditemukan")
+            }
+            Log.d("detaillapangan", "getDetailFromFireStore: $detail")
+
+        } catch (e:FirebaseFirestoreException) {
+            Log.d("error", "getDetailFromFireStore: $e")
+        }
+
+        return detail
     }
 }
 
-suspend fun getDetailFromFireStore():detaillapangan {
-    val db = FirebaseFirestore.getInstance()
-    var detail = detaillapangan()
-
-    try {
-        db.collection("detail").get().await().map {
-            val result = it.toObject(detaillapangan::class.java)
-            detail = result
-        }
-    } catch (e:FirebaseFirestoreException) {
-        Log.d("error", "getDetailFromFireStore: $e")
-    }
-
-    return detail
-}
