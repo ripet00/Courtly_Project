e07e2448b3b3fd775c127643e11a5d7731f8ac06
Rifat <rifat.khaidir@gmail.com>
2024-12-05 16:12:46 +0800

    build: add route detaillapangan & pass the lapanganId from homepage_vm

--- app/src/main/java/com/example/courtlyproject/controller/MyAppNavigation.kt
+++ app/src/main/java/com/example/courtlyproject/controller/MyAppNavigation.kt
@@ -2,16 +2,17 @@ package com.example.courtlyproject.controller
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.courtlyproject.Feature.auth.AuthViewModel
 import com.example.courtlyproject.Feature.auth.LoginPage
 import com.example.courtlyproject.Feature.auth.SignupPage
 import com.example.courtlyproject.Feature.auth.WelcomingPage
+import com.example.courtlyproject.Feature.detail.view.HomeScreen
 import com.example.courtlyproject.Feature.detail.view.detailcontent
-import com.example.courtlyproject.view.HomeScreen
 import com.example.courtlyproject.view.MainActivity
 import com.example.courtlyproject.view.SplashScreen
 
@@ -35,8 +36,16 @@ fun MyAppNavigation(modifier: Modifier = Modifier, authViewModel: AuthViewModel)
         composable("homepage") {
             HomeScreen( navController,authViewModel)
         }
-        composable("detail") {
-            detailcontent(navController = navController, detaillapanganViewModel = viewModel())
+        composable(
+            route = "detail/{lapanganId}",
+            arguments = listOf(navArgument("lapanganId") {
+                type = NavType.StringType
+            })
+        ){
+            // proses passing id
+            backStackEntry ->
+            val lapanganId = backStackEntry.arguments?.getString("lapanganId")
+            detailcontent(navController = navController, lapanganId = lapanganId.toString())
         }
     }
 }
